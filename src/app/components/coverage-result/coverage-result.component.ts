9724cd1474885b82bb48a0cbf3feb80b36fde14d
Martín Caprino <caprinoma@globalhitss.com>
2020-12-15 11:39:58 -0300

    changed style -> using jumbotron, added messages and icons

--- src/app/components/coverage-result/coverage-result.component.ts
+++ src/app/components/coverage-result/coverage-result.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+const COVERED_MSG = 'Con cobertura de despacho';
+const NOT_COVERED_MSG = 'Por el momento no es posible la entrega debido a un aumento de casos registrados de COVID-19 en esta zona';
 @Component({
   selector: 'app-coverage-result',
   templateUrl: './coverage-result.component.html',
@@ -9,7 +10,9 @@ import { ActivatedRoute } from '@angular/router';
 
 export class CoverageResultComponent implements OnInit {
 
-  cobertura: boolean;
+  cobertura: string;
+  message: string;
+  imageName: string;
 
   constructor(
     private route: ActivatedRoute
@@ -18,6 +21,13 @@ export class CoverageResultComponent implements OnInit {
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.cobertura = params['cobertura'];
+      if(this.cobertura === 'true'){
+        this.message = COVERED_MSG;
+        this.imageName = 'ok_icon.png';
+      }else{
+        this.message = NOT_COVERED_MSG;
+        this.imageName = 'error_icon.png'; 
+      }
     });
   }
 
