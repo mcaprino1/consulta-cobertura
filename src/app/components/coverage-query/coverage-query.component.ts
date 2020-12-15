5df6d51e98252f7f1959d77948acf93b5d21c1e8
Martín Caprino <caprinoma@globalhitss.com>
2020-12-14 21:14:19 -0300

    added counties select, button style, select placeholders and event listeners

--- src/app/components/coverage-query/coverage-query.component.ts
+++ src/app/components/coverage-query/coverage-query.component.ts
@@ -11,24 +11,31 @@ import { CoverageService } from 'src/app/services/coverage/coverage-service';
 export class CoverageQueryComponent implements OnInit {
   regiones: Array<RegionModel>;
   comunas: Array<CountyModel>;
+  regionSelected: boolean;
 
   constructor(
     private service: CoverageService
-  ) { 
+  ) { }
+
+  populateCounties(selectedRegion: string): void {
+    console.log('retrieving selected region option: ' + selectedRegion);
+    this.regionSelected = true;    
+    this.comunas = new Array<CountyModel>();
+    this.service.obtenerComunas(selectedRegion)
+    .then(
+      (data) => data.coverageAreas.forEach(
+        r => this.comunas.push(new CountyModel(r.countyCode, r.coverageName, r.ineCountyCode, r.regionCode))
+    ));
+  }
+
+  ngOnInit(): void {
+    this.regionSelected = false;
     this.regiones = new Array<RegionModel>();
     this.service.obtenerRegiones()
     .then(
       (data) => data.regions.forEach(
         r => this.regiones.push(new RegionModel(r.regionId, r.regionName, r.ineRegionCode))
     ));
-  }
-
-  populateCountiesSelect(selectedRegion: string): void {
-    console.log('retrieving selected region option: ' + selectedRegion);
-  }
-
-  ngOnInit(): void {
-    
   }
 
 }
