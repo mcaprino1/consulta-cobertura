0266ea8f388b17361868be1fb6ede06203a8313b
Martín Caprino <caprinoma@globalhitss.com>
2020-12-15 04:37:46 -0300

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 import { CoverageQueryComponent } from './components/coverage-query/coverage-query.component';
 import { CoverageResultComponent } from './components/coverage-result/coverage-result.component';
 import { FormsModule } from '@angular/forms';
@@ -10,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     CoverageQueryComponent,
     CoverageResultComponent
   ],
